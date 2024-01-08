3f1c4364be16be276ee0773b3ac13498bd161528
Burtton013 <cbop95@gmail.com>
2024-01-08 17:54:22 -0600

    first commit

---
+++ ejercicio.js
@@ -0,0 +1,157 @@
+//Tipo de dato Number
+
+let fechaDeNacimiento = 1995;
+console.log(fechaDeNacimiento);
+
+let cantidadDeAutos = 0;
+console.log(cantidadDeAutos);
+
+let cantidadDeDedos = 20;
+console.log(cantidadDeDedos);
+
+let numeroDeUsuario = 5;
+console.log(numeroDeUsuario);
+
+let materiasAprobadas = 10;
+console.log(materiasAprobadas);
+
+let materiasReprobadas = 5;
+console.log(materiasReprobadas);
+
+let añosVivo = 28;
+console.log (añosVivo);
+
+let tazasRotas = 3;
+console.log(tazasRotas);
+
+let museosVisitados= 4;
+console.log(museosVisitados);
+
+let tareasPendientes= 6;
+console.log(tareasPendientes);
+
+//Tipo de dato String
+
+let curp = "ajsuie9300-23o";
+console.log(curp);
+
+let correoElectronico = "cbp45@gmail.com";
+console.log(correoElectronico);
+
+let placasAuto = "cde-456";
+console.log(placasAuto);
+
+let colonia = "Ignacio Fregoso";
+console.log(colonia);
+
+let delegacion = "Milpa Juarez";
+console.log(delegacion);
+
+let clave = "4673ydkeie@#jd";
+console.log(clave);
+
+let nombreId = "Burtton013";
+console.log(nombreId);
+
+let padronElectoral = "938494";
+console.log(padronElectoral);
+
+let nacionalidad = "Mexicano";
+console.log(nacionalidad);
+
+//Tipo de dato Boolean
+
+let servicioMilitar = true;
+console.log(servicioMilitar);
+
+let titulado = true;
+console.log(titulado);
+
+let carreaTrunca = false;
+console.log(carreaTrunca);
+
+let siguesAqui = true;
+console.log(siguesAqui);
+
+let eresHumano = false;
+console.log(eresHumano);
+
+let eresAlien = true;
+console.log(eresAlien);
+
+let eresSintiente = true;
+console.log(eresSintiente);
+
+let nacionalidadMexicana = true;
+console.log(nacionalidadMexicana);
+
+let empleado = false;
+console.log(empleado);
+
+let soltero = true;
+console.log (soltero);
+
+let jubilado = false;
+console.log(jubilado);
+
+//Declarar variables numericas y realizar suma, resta, multiplicacion, division.
+
+let edadJulian = 33;
+let edadPablo = 456;
+let sum = edadJulian + edadPablo;
+console.log("La suma de las edades de Julian y Pablo nos da un total de: " + sum);
+
+let cantidadAlumnos = 30;
+let rebanadasPastel = 120;
+let div = rebanadasPastel / cantidadAlumnos ;
+console.log ("cada alumno recibe: " + div + " rebanadas de pastel");
+
+let alumnosPorSalon = 50;
+let cantidadSalones= 6;
+let multip = cantidadSalones * alumnosPorSalon;
+console.log ("La cantidad total de alumnos en todo el plantes es de: " + multip);
+
+let dineroTotal = 45693;
+let deudaTotal = 23543;
+let dineroSobrante = dineroTotal - deudaTotal;
+console.log ("La cantidad de dinero restante despues de pagar la deuda es de: " + dineroSobrante);
+
+//Declarar variables que concatenen cadenas de texto
+
+let edadJulio = 30;
+console.log("La edad de Julian es: " + edadJulian);
+
+let visitasMes = 34;
+console.log("La cantidad de visitas al mes en tu canal de youtube es de: " + visitasMes);
+
+let vidJuegoFav = "Bioshock 1 y 3";
+console.log("Mi juego favorito es: " + vidJuegoFav);
+
+let musicFormatFav = " FLAC ";
+console.log( "Cual es tu formato favorito de musica: " + musicFormatFav);
+
+let musicGenrFav = "Metal";
+console.log("El genero de musica favorita del usuario es: " + musicGenrFav);
+
+// Declarar variables y convertirlas a otro tipo de dato
+
+let mascotaNombre = "Pistacho";
+let nombrePropietario = "David";
+let mascotaId = 456;
+let seguroVida = true;
+let estatus = "vivo";
+
+let mascotaIdString = String(mascotaId);
+console.log(mascotaIdString);
+
+let mascotaNombreNumerico = Number(mascotaNombre);
+console.log(mascotaNombreNumerico)
+
+let seguroVidaBoolean = Number (seguroVida);
+console.log(seguroVidaBoolean);
+
+let estatusBoolean = Boolean(estatus);
+console.log(estatusBoolean);
+
+let nombrePropietarioBoolean = Boolean (nombrePropietario);
+console.log(nombrePropietarioBoolean);
