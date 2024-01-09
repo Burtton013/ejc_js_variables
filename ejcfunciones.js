90012885389a34c53ecda92125641985a1c3d9e7
Burtton013 <cbop95@gmail.com>
2024-01-09 13:59:16 -0600

    Se agrego ejercicio de funciones

---
+++ ejcfunciones.js
@@ -0,0 +1,50 @@
+//Declarar una funcion para cada problema planteado
+
+//Calcular el perimetro de un circulo
+
+function perimetroCirculo (d){
+    return 3.14 * d
+}
+
+const perimetroCirculoFlecha = (d) => (3.14 * d)
+
+
+//calcular el area de un rectangulo
+
+function areaRectangulo (l, a){
+    return l*a
+}
+
+const areaRectanguloFlecha = (l, a) => (l * a)
+
+//Calcular el cuadrado de un numero
+
+function numeroCuadrado (x){
+    return x**2
+}
+
+const numeroCuadradoFlecha = (x) => (x**2)
+
+//Calcular la conversion de grados celsius a farenheit
+
+function celsiusFarenheit (c){
+    return c * 1.8 + 32
+}
+
+const celsiusFarenheitFlecha = (c) => (c*1.8 + 32)
+
+//Calcular el valor del voltaje dadas la resistencia y la corriente
+
+function voltaje (amp , ohm) {
+    return amp * ohm
+}
+
+const voltajeFlecha = (amp, ohm) => (amp * ohm)
+
+//Calcular el volumen de una esfera
+
+function volEsfera (r){
+    return 3/4*3.14*r**3
+}
+
+const volEsferaFlecha = (r) => (3/4*3.14*r**3)
